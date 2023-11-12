927cb8bc1bb8d2f2d659b8ffd7c9aa8062931555
Maiki02 <miqueasdavidgentile@gmail.com>
2023-11-12 02:06:29 -0300

    Set colors

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NAVEGATION } from 'src/app/shared/const/navegation';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  public NAVEGATION=NAVEGATION;
+  constructor(private router:Router){}
 
+  goTo(route:string){
+    this.router.navigate(['/'+route]);
+  }
 }
